d42dd74c3f3a832778f451f288f0af07a2b4a8f4
Ivan Ludvig <ivanludvigdev@gmail.com>
2023-11-19 00:03:09 +0100

    copy buttons

--- webapp/src/components/CreateMementoForm/LoadingDialog/CopyButton/CopyButton.tsx
+++ webapp/src/components/CreateMementoForm/LoadingDialog/CopyButton/CopyButton.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 export default function CopyButton({ copyContent }: { copyContent: string }) {
+    const [clicked, setIsClicked] = useState(false);
+
     return (
         <button
             className={
-                'bg-primary w-[78px] h-[40px] text-black rounded-r-[12px] transition-colors ease-in-out duration-300 hover:bg-[#fff]'
+                'bg-primary w-[84px] h-[40px] text-black rounded-r-[12px] transition-colors ease-in-out duration-300 hover:bg-[#fff]'
                 + ' absolute top-[0px] right-[0px]'
             }
-            onClick={() => navigator.clipboard.writeText(copyContent)}
+            onClick={() => { navigator.clipboard.writeText(copyContent); setIsClicked(true); }}
         >
-            Copy
+            {!clicked ? 'Copy' : 'Copied'}
         </button>
     )
 }
