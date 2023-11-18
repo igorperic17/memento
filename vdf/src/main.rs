a467f1217ec26f41a1d3e116f89bf05419d790d3
Igor Peric <igorperic@live.com>
2023-11-18 17:45:33 +0300

    Encription endpoints work

--- vdf/src/main.rs
+++ vdf/src/main.rs
@@ -1,7 +1,8 @@
-use std::time::Instant;
-
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use num_bigint::BigUint;
 use num_traits::{One, Zero};
+use serde_derive::{Deserialize, Serialize};
+use std::time::Instant;
 
 fn mod_pow(base: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
     let mut result = BigUint::one();
@@ -50,7 +51,6 @@ fn mod_inverse(a: &BigUint, m: &BigUint) -> BigUint {
     x % m
 }
 
-
 fn forward_computation(x: &BigUint, e: &BigUint, m: &BigUint, steps: u32) -> BigUint {
     let mut result = x.clone();
     for _ in 0..steps {
@@ -67,26 +67,70 @@ fn backward_computation(y: &BigUint, d: &BigUint, m: &BigUint, steps: u32) -> Bi
     result
 }
 
-fn main() {
-    let x = BigUint::parse_bytes(b"12345", 10).unwrap();
-    let e = BigUint::parse_bytes(b"3", 10).unwrap();
-    let m = BigUint::parse_bytes(b"1000000007", 10).unwrap();
-    let steps = 1000000;
-
-    // Measure forward computation time
-    let start_time = Instant::now();
-    let forward_result = forward_computation(&x, &e, &m, steps);
-    let forward_duration = start_time.elapsed();
-    println!("Forward Result: {}", forward_result);
-    println!("Time taken for forward computation: {:?}", forward_duration);
-
-    let phi = m.clone() - BigUint::one();
-    let d = mod_inverse(&e, &phi);
-
-    // Measure backward computation time
-    let start_time = Instant::now();
-    let backward_result = backward_computation(&forward_result, &d, &m, steps);
-    let backward_duration = start_time.elapsed();
-    println!("Backward Result: {}", backward_result);
-    println!("Time taken for backward computation: {:?}", backward_duration);
+#[derive(Deserialize)]
+struct ComputationRequest {
+    x: String,
+    e: String,
+    m: String,
+    steps: u32,
 }
+
+#[derive(Serialize)]
+struct ComputationResponse {
+    result: String,
+    computation_time: String,
+}
+
+async fn forward(request: web::Json<ComputationRequest>) -> impl Responder {
+    let x = BigUint::parse_bytes(request.x.as_bytes(), 10).unwrap();
+    let e = BigUint::parse_bytes(request.e.as_bytes(), 10).unwrap();
+    let m = BigUint::parse_bytes(request.m.as_bytes(), 10).unwrap();
+
+    let start_time = Instant::now();
+    let result = forward_computation(&x, &e, &m, request.steps);
+    let duration = start_time.elapsed();
+
+    HttpResponse::Ok().json(ComputationResponse {
+        result: result.to_str_radix(10),
+        computation_time: format!("{:?}", duration),
+    })
+}
+
+async fn backward(request: web::Json<ComputationRequest>) -> impl Responder {
+    let y = BigUint::parse_bytes(request.x.as_bytes(), 10).unwrap();
+    let e = BigUint::parse_bytes(request.e.as_bytes(), 10).unwrap();
+    let m = BigUint::parse_bytes(request.m.as_bytes(), 10).unwrap();
+
+    let phi = m.clone() - BigUint::one(); // Assuming 'm' is the modulus for the inverse calculation
+    let d = mod_inverse(&e, &phi);        // Calculate the modular inverse of 'e'
+
+    let start_time = Instant::now();
+    let result = backward_computation(&y, &d, &m, request.steps);
+    let duration = start_time.elapsed();
+
+    HttpResponse::Ok().json(ComputationResponse {
+        result: result.to_str_radix(10),
+        computation_time: format!("{:?}", duration),
+    })
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/forward", web::post().to(forward))
+            .route("/backward", web::post().to(backward))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+// example usage (from terminal):
+
+// curl -X POST http://localhost:8080/forward -H "Content-Type: application/json" -d '{"x": "12345", "e": "3", "m": "1000000007", "steps": 1000000}'
+// {"result":"353814440","computation_time":"5.225790833s"}
+
+// curl -X POST http://localhost:8080/backward -H "Content-Type: application/json" -d '{"x": "353814440", "e": "3", "m": "1000000007", "steps": 1000000}'
+// {"result":"12345","computation_time":"57.650758875s"}
